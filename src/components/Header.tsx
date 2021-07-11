7d50ecc7aab2e92ab1a2a75d3359daaf8701eb7c
jpaul121 <jpaulvalen+github@gmail.com>
2021-07-10 20:15:37 -0400

    Add icons, contact info to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,6 @@
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
 function Header() {
@@ -6,34 +9,28 @@ function Header() {
       <nav className='container mx-auto px-6 md:px-12 py-4'>
           <div className='md:flex justify-between items-center'>
             <div className='flex justify-between items-center'>
-              <a href='#' className='text-white'>
-                <svg className='w-8 mr-2 fill-current' xmlns='http://www.w3.org/2000/svg' data-name='Capa 1' viewBox='0 0 16.16 12.57'>
-                  <defs>
-                  </defs>
-                  <path d='M14.02 4.77v7.8H9.33V8.8h-2.5v3.77H2.14v-7.8h11.88z'>
-                  </path>
-                  <path d='M16.16 5.82H0L8.08 0l8.08 5.82z'>
-                  </path>
-                </svg>
-              </a>
-              <div className='md:hidden'>
-                <button className='text-white focus:outline-none'>
-                  <svg className='h-12 w-12' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                    <path d='M4 6H20M4 12H20M4 18H20' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'>
-                    </path>
-                  </svg>
-                </button>
-              </div>
+              <span className='font-bold uppercase text-white'>
+                compress.it
+              </span>
             </div>
             <div className='hidden md:flex items-center'>
-              <a className='text-lg uppercase mx-3 text-white cursor-pointer hover:text-gray-300'>
-                About us
+              <a
+                className='text-lg uppercase mx-3 text-white cursor-pointer hover:text-gray-300'
+                href='mailto: jpvalencia@alumni.harvard.edu'
+              >
+                Contact
               </a>
-              <a className='text-lg uppercase mx-3 text-white cursor-pointer hover:text-gray-300'>
-                Calendar
+              <a
+                className='text-3xl uppercase mx-3 text-white cursor-pointer hover:text-gray-300'
+                href='https://github.com/jpaul121/'
+              >
+                <FontAwesomeIcon icon={faGithub} />
               </a>
-              <a className='text-lg uppercase mx-3 text-white cursor-pointer hover:text-gray-300'>
-                Contact us
+              <a
+                className='text-3xl uppercase mx-3 text-white cursor-pointer hover:text-gray-300'
+                href='https://linkedin.com/in/jpaulvalen/'
+              >
+                <FontAwesomeIcon icon={faLinkedin} />
               </a>
             </div>
         </div>
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,9 +3,6 @@ import React from 'react'
 function HeroSection() {
   return (
     <div className='lg:w-3/5 xl:w-2/5 flex flex-col items-start relative z-10'>
-      <span className='font-bold uppercase text-yellow-400'>
-        compress.it
-      </span>
       <h1 className='font-bold text-6xl sm:text-7xl text-white leading-tight mt-4'>
         Compress images online
         <br/>
